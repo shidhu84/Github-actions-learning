fb2389d558093217c23e97f39e67409cd9b029a0
shidhu84 <yenureshid@gmail.com>
2025-12-30 08:39:25 +0000

    added substract function and test

--- src/addition.py
+++ src/addition.py
@@ -3,6 +3,14 @@
 def add(a, b):
     return a + b
 
+def subtract(a, b): 
+    return a - b
+
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+def test_subtract(): 
+    assert subtract(5, 3) == 2
+    assert subtract(2, 5) == -3
+    assert subtract(0, 0) == 0
